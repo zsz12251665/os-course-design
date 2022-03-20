b5e7e857261293260501df03d34132621b02c2f6
zsz12251665 <zsz12251665@users.noreply.github.com>
2022-03-20 20:48:31 +0800

    feat: Implement commands

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
 #include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
 #include "lib/storage.h"
 #include "lib/inode.h"
 #include "cmd.h"
@@ -15,17 +18,65 @@ int main() {
 	printf("\n");
 
 	// Initializers
+	srand(time(NULL));
 	storageInitializer();
 	inodeInitializer();
+	commandInitializer();
 
 	// Main Program
-	cmd::sum();
-	INode root = selectINode(1);
-	printf("%lu\n", sizeof root);
-	printf("%d %d %d %llu\n", root.num, root.type, root.size, root.create_time);
-	for (int i = 0; i < 10; ++i)
-		printf("%d ", root.direct_addr[i]);
-	printf("%d\n", root.indirect_addr);
+	for (;;) {
+		printf("$ ");
+		char command[PATH_MAX_LENGTH];
+		if (scanf("%s", command) == EOF) {
+			putchar('\n');
+			break;
+		}
+		if (strcmp(command, "createFile") == 0) {
+			char filename[PATH_MAX_LENGTH];
+			int size;
+			scanf("%s%d", filename, &size);
+			cmd::createFile(filename, size);
+		} else
+		if (strcmp(command, "deleteFile") == 0) {
+			char filename[PATH_MAX_LENGTH];
+			scanf("%s", filename);
+			cmd::deleteFile(filename);
+		} else
+		if (strcmp(command, "createDir") == 0) {
+			char dirname[PATH_MAX_LENGTH];
+			scanf("%s", dirname);
+			cmd::createDir(dirname);
+		} else
+		if (strcmp(command, "deleteDir") == 0) {
+			char dirname[PATH_MAX_LENGTH];
+			scanf("%s", dirname);
+			cmd::deleteDir(dirname);
+		} else
+		if (strcmp(command, "changeDir") == 0) {
+			char dirname[PATH_MAX_LENGTH];
+			scanf("%s", dirname);
+			cmd::changeDir(dirname);
+		} else
+		if (strcmp(command, "dir") == 0) {
+			cmd::dir();
+		} else
+		if (strcmp(command, "cp") == 0) {
+			char src[PATH_MAX_LENGTH], des[PATH_MAX_LENGTH];
+			scanf("%s%s", src, des);
+			cmd::cp(src, des);
+		} else
+		if (strcmp(command, "sum") == 0) {
+			cmd::sum();
+		} else
+		if (strcmp(command, "cat") == 0) {
+			char filename[PATH_MAX_LENGTH];
+			scanf("%s", filename);
+			cmd::cat(filename);
+		} else {
+			printf("Error: Unknown command \"%s\"!\n", command);
+		}
+		while(getchar() != '\n'); // Clear the rest of the line in stdin
+	}
 
 	// Destructors
 	storageDestructor();
