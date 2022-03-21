ae36ce1e95943e3d27a41cb780d1d8e73c6c1ca0
whaohan <2778351280@qq.com>
2022-03-21 19:35:48 +0800

    feat: complete file ops

--- lib/file.cpp
+++ lib/file.cpp
@@ -58,6 +58,7 @@ INode createFile(INode cwd, const char* path) {
     INode file_inode = createINode();
     INode dir_inode = getDirINode(cwd, path);
     AddDirEntry(dir_inode, getFilename(path), file_inode.num);
+    return file_inode;
 }
 
 bool fillFile(INode& file_inode, int size) {
@@ -67,9 +68,9 @@ bool fillFile(INode& file_inode, int size) {
     updateINode(file_inode);
     
     int data_addr, addr_addr;
+    char random_data[BLOCK_SIZE];
     for (int i = 0; i < size / BLOCK_SIZE; ++i) {
         data_addr = findFreeBlock();
-        char random_data[BLOCK_SIZE];
         for(int j = 0; j < BLOCK_SIZE; ++j) {
             random_data[i] = char(rand() % (127 - 32) + 32); // 32 - 126
         }
@@ -89,11 +90,65 @@ bool fillFile(INode& file_inode, int size) {
 }
 
 bool removeFile(INode cwd, const char* path) {
+    INode file_inode = getFileINode(cwd, path);
+    // free data block
+    for (int i = 0; i < 10; ++i) {
+        if (file_inode.direct_addr[i] != 0) {
+            freeBlock(file_inode.direct_addr[i]);
+        }
+    }
+    int pDes[BLOCK_SIZE / 4];
+    if (file_inode.indirect_addr != 0 && 
+        getBlock(file_inode.indirect_addr, pDes) == 0) {
+        for (int i = 0; i < BLOCK_SIZE / 4; ++i) {
+            if (pDes[i] != 0)
+                freeBlock(pDes[i]);
+        }
+    }
 
+    INode dir_inode = getDirINode(cwd, path);
+    // delete dir entry
+    RemoveDirEntry(dir_inode, file_inode.num);
+
+    // free inode
+    deleteINode(file_inode.num);
+
+    return 0;
 }
 
 bool copyFile(const INode& src_inode, INode& des_inode) {
+    int size = src_inode.size;
+    des_inode.size = size;
+    des_inode.type = src_inode.type;
+    updateINode(des_inode);
+    
+    int data_addr, addr_addr;
+    char src_data[BLOCK_SIZE];
+    int pDes[BLOCK_SIZE / 4];
+    if (size / BLOCK_SIZE >= 10) {
+        getBlock(src_inode.indirect_addr, pDes);
+    }
 
+    for (int i = 0; i < size / BLOCK_SIZE; ++i) {
+        data_addr = findFreeBlock();
+        // read data from src
+        if (i < 10) {
+            getBlock(src_inode.direct_addr[i], src_data);
+            putBlock(data_addr, src_data);
+            des_inode.direct_addr[i] = data_addr;
+        } else if (i == 10) {
+            getBlock(pDes[i - 10], src_data);
+            putBlock(data_addr, src_data);
+            int addr_addr = findFreeBlock();
+            putBlock(addr_addr, &data_addr, 4);
+            des_inode.indirect_addr = addr_addr;
+        } else {
+            getBlock(pDes[i - 10], src_data);
+            putBlock(data_addr, src_data);
+            putBlock(des_inode.indirect_addr + 4 * (i - 10), &data_addr, 4);
+        }
+    }
+    return 0;
 }
 
 
